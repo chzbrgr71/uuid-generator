b17b5faa5fe163e06fad3b6012ef655c96e126aa
Brian Redmond <brianisrunning@gmail.com>
2017-11-06 07:05:43 -0500

    added test suite

--- brigade.js
+++ brigade.js
@@ -1,5 +1,21 @@
-const { events } = require("brigadier")
+const { events, Job, Group } = require('brigadier')
 
 events.on("push", function(e, project) {
-    console.log("chzbrgr71 received push for commit " + e.commit)
+    console.log("received push for commit " + e.commit)
+  
+    // Create a new job
+    var node = new Job("test-runner")
+  
+    // We want our job to run the stock Docker Python 3 image
+    node.image = "python:3"
+  
+    // Now we want it to run these commands in order:
+    node.tasks = [
+      "cd /src/",
+      "pip install -r requirements.txt",
+      "python setup.py test"
+    ]
+  
+    // We're done configuring, so we run the job
+    node.run()
   })
